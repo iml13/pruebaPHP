caf6b2b16daa9cef1667bb2034a23950afe8007b
iml13 <i13morenolabrador@gmail.com>
2017-11-18 03:48:45 +0100

    pruebaPHP

---
+++ adios.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DWES_UD2_CP2</title>
+  </head>
+  <body>
+    <?php echo "Hasta luego desarrollador!!!!" ?>
+  </body>
+</html>
---
+++ hola.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Paco es un campeón!!!!" ?>
+  </body>
+</html>
